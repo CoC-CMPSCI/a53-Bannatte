4f65014f20b7a2f6ff4e53e0c99d150a1953b8f5
Bannatte <32626837+Bannatte@users.noreply.github.com>
2026-04-01 00:22:18 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main() {
     long long begin, end;
-    long long summation = 0;
 
     // TODO: prompt user for begin and end values
     // cout << "Enter begin and end values: ";
     cin >> begin >> end;
+    long long summation = 0;
+    
         
     // TODO: loop from begin to end and accumulate sum of even values
     for (long long i = begin; i <= end; ++i) {
