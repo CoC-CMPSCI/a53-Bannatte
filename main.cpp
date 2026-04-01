62b1d0921dbdd3a0b00f42527dcc66539d8b08de
Bannatte <32626837+Bannatte@users.noreply.github.com>
2026-03-31 23:41:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,16 @@ int main()
     int i, begin, end, sum = 0;
 
     // TODO: prompt user for begin and end values
+    cout << "Enter begin and end values: ";
+    cin >> begin >> end;
+    
     // TODO: loop from begin to end and accumulate sum of even values
+    for (int i = begin; i <= end; ++i) {
+        if (i % 2 == 0) {
+            sum += i;
+        }
+    }
+
+    cout << "The sum of even values is " << sum << endl;
+    return 0;
 }
