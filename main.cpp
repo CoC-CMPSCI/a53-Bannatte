f673309a2116ff1300eb3412f6f4b5e32a142427
Bannatte <32626837+Bannatte@users.noreply.github.com>
2026-04-01 00:11:25 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main() {
-    int i, begin, end, sum = 0;
+    long long begin, end, sum = 0;
 
     cin >> begin >> end;
-    
-    /*
-    while (true) {
-        cout << "Enter begin and end values: ";
-        cin >> begin >> end;
-        if (begin <= end) {
-            break;
-        }
-    }
-    */
 
-    for (int i = begin; i <= end; ++i) {
+    for (long long i = begin; i <= end; ++i) {
         if (i % 2 == 0) {
             sum += i;
         }
     }
 
     cout << "The sum of even values is " << sum << endl;
+
     return 0;
 }
